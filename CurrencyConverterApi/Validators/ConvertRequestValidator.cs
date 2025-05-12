dea5c9909c848d63bbc02ac257019e1f7bc91599
zabhi10 <55391124+zabhi10@users.noreply.github.com>
2025-05-12 11:49:03 +0530

    Update ConvertRequestValidator.cs

--- CurrencyConverterApi/Validators/ConvertRequestValidator.cs
+++ CurrencyConverterApi/Validators/ConvertRequestValidator.cs
@@ -5,25 +5,30 @@ namespace CurrencyConverterApi.Validators
 {
     public class ConvertRequestValidator : AbstractValidator<ConvertRequest>
     {
+        private readonly List<string> _excludedCurrencies = new List<string> { "TRY", "PLN", "THB", "MXN" };
         public ConvertRequestValidator()
         {
             RuleFor(x => x.BaseCurrency)
-                .NotEmpty().WithMessage("Base currency is required.")
-                .Length(3).WithMessage("Base currency must be 3 characters long.")
-                .Matches("^[a-zA-Z]+$").WithMessage("Base currency must contain only characters.");
+            .NotEmpty().WithMessage("Base currency is required.")
+            .Length(3).WithMessage("Base currency must be 3 characters long.")
+            .Matches("^[a-zA-Z]+$").WithMessage("Base currency must contain only characters.")
+            .Must(currency => currency != null && !_excludedCurrencies.Contains(currency.ToUpper()))
+            .WithMessage(x => $"Base currency '{x.BaseCurrency}' is not supported.");
 
             RuleFor(x => x.TargetCurrency)
-                .NotEmpty().WithMessage("Target currency is required.")
-                .Length(3).WithMessage("Target currency must be 3 characters long.")
-                .Matches("^[a-zA-Z]+$").WithMessage("Target currency must contain only characters.");
+            .NotEmpty().WithMessage("Target currency is required.")
+            .Length(3).WithMessage("Target currency must be 3 characters long.")
+            .Matches("^[a-zA-Z]+$").WithMessage("Target currency must contain only characters.")
+            .Must(currency => currency != null && !_excludedCurrencies.Contains(currency.ToUpper()))
+            .WithMessage(x => $"Target currency '{x.TargetCurrency}' is not supported.");
 
             RuleFor(x => x.Amount)
-                .GreaterThan(0).WithMessage("Amount must be greater than zero.");
+            .GreaterThan(0).WithMessage("Amount must be greater than zero.");
 
             RuleFor(x => x)
-                .Must(x => x.BaseCurrency != x.TargetCurrency)
-                .WithMessage("Base currency and target currency cannot be the same.")
-                .When(x => !string.IsNullOrEmpty(x.BaseCurrency) && !string.IsNullOrEmpty(x.TargetCurrency) && x.BaseCurrency.Length == 3 && x.TargetCurrency.Length == 3);
+            .Must(x => x.BaseCurrency != x.TargetCurrency)
+            .WithMessage("Base currency and target currency cannot be the same.")
+            .When(x => !string.IsNullOrEmpty(x.BaseCurrency) && !string.IsNullOrEmpty(x.TargetCurrency) && x.BaseCurrency.Length == 3 && x.TargetCurrency.Length == 3);
         }
     }
 }
